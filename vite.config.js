d5a039db478dd8e5a24715bffd4a431f1c9a3cd0
SchubertChris <schubert_chris@ocketmail.com>
2025-02-07 18:49:16 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: '/OnlineShop/', // Replace <REPO_NAME> with the name of your GitHub repository
   plugins: [react()],
 });
